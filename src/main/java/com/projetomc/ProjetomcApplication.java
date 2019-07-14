3f561468c8f26bccadaa3f306f241fbef5dcef94
Maks Silva <maksuedson@hotmail.com>
2019-07-14 19:40:35 -0300

    Criando operacao de instaciacao

--- src/main/java/com/projetomc/ProjetomcApplication.java
+++ src/main/java/com/projetomc/ProjetomcApplication.java
@@ -1,13 +1,32 @@
 package com.projetomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.projetomc.domain.Categoria;
+import com.projetomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetomcApplication {
+public class ProjetomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
