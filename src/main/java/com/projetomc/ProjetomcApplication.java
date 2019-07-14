181fdc4a6880d363e9c708366e71a036ffee2004
Maks Silva <maksuedson@hotmail.com>
2019-07-13 21:32:19 -0300

    Projeto criado

---
+++ src/main/java/com/projetomc/ProjetomcApplication.java
@@ -0,0 +1,13 @@
+package com.projetomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomcApplication.class, args);
+	}
+
+}
