6e6c4ea3ca8b7a781223b285255fa053b663d9d7
Maks Silva <maksuedson@hotmail.com>
2019-07-14 16:23:02 -0300

    Banco de dados H2  e criacao automaitca da base de dados

--- src/main/java/com/projetomc/domain/Categoria.java
+++ src/main/java/com/projetomc/domain/Categoria.java
@@ -2,9 +2,17 @@ package com.projetomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
-private static final long serialVersionUID = 1L;
-	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
@@ -62,6 +70,5 @@ private static final long serialVersionUID = 1L;
 	public String toString() {
 		return "Categoria [id=" + id + ", nome=" + nome + "]";
 	}
-	
-	
+
 }
