07fa70e6a47fc0a487bb6bd616b019fe980181d9
Maks Silva <maksuedson@hotmail.com>
2019-07-14 18:44:35 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/projetomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.projetomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetomc.domain.Categoria;
+import com.projetomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
