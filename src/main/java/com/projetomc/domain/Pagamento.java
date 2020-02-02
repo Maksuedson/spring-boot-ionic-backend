10b4704fbac9c79db405090b092f9dd51b5c62dd
Maksuedson <maksuedson@hotmail.com>
2020-02-02 13:53:39 -0300

    Pedido, EstadoPagamento e Pagamento - Parte 2 (um para um)

--- src/main/java/com/projetomc/domain/Pagamento.java
+++ src/main/java/com/projetomc/domain/Pagamento.java
@@ -2,14 +2,25 @@ package com.projetomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
 import com.projetomc.domain.enums.EstadoPagamento;
 
+@Entity
 public class Pagamento implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Integer id;
 	private EstadoPagamento estado;
 	
+	@OneToOne
+	@JoinColumn(name ="pedido_id")
+	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {		
