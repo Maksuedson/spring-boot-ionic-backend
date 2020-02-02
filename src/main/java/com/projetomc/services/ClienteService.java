c1e25fef54dfe5b4671ee7fe5f0c73b9d3bf696d
Maksuedson <maksuedson@hotmail.com>
2020-02-02 07:13:23 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/projetomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.projetomc.services;
+
+import java.util.Optional;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetomc.domain.Cliente;
+import com.projetomc.repositories.ClienteRepository;
+import com.projetomc.services.exceptions.ObejctNotFoundException;
+
+
+@Service
+public class ClienteService {
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObejctNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));		
+	}
+}
