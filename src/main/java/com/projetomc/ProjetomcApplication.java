6d3b047244d9e1b14f6719a14e74975832ce2e62
Maksuedson <maksuedson@hotmail.com>
2020-02-14 06:21:48 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/projetomc/ProjetomcApplication.java
+++ src/main/java/com/projetomc/ProjetomcApplication.java
@@ -13,6 +13,7 @@ import com.projetomc.domain.Cidade;
 import com.projetomc.domain.Cliente;
 import com.projetomc.domain.Endereco;
 import com.projetomc.domain.Estado;
+import com.projetomc.domain.ItemPedido;
 import com.projetomc.domain.Pagamento;
 import com.projetomc.domain.PagamentoComBoleto;
 import com.projetomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.projetomc.repositories.CidadeRepository;
 import com.projetomc.repositories.ClienteRepository;
 import com.projetomc.repositories.EnderecoRepository;
 import com.projetomc.repositories.EstadoRepository;
+import com.projetomc.repositories.ItemPedidoRepository;
 import com.projetomc.repositories.PagamentoRepository;
 import com.projetomc.repositories.PedidoRepository;
 import com.projetomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class ProjetomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
@@ -121,7 +126,19 @@ public class ProjetomcApplication implements CommandLineRunner{
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
-
-	}
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+	}	
 
 }
--- src/main/java/com/projetomc/domain/Pedido.java
+++ src/main/java/com/projetomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.projetomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -12,6 +14,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -34,6 +37,9 @@ public class Pedido implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoEntrega;
+	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>(); 
 	
 	public Pedido() {		
 	}
@@ -84,6 +90,14 @@ public class Pedido implements Serializable {
 
 	public void setEnderecoEntrega(Endereco enderecoEntrega) {
 		this.enderecoEntrega = enderecoEntrega;
+	}		
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
