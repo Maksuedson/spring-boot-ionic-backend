709917df10da1f80871709d101fd3603033b09b3
Maksuedson <maksuedson@hotmail.com>
2020-03-14 20:14:17 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/projetomc/domain/Categoria.java
+++ src/main/java/com/projetomc/domain/Categoria.java
@@ -9,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity 
 public class Categoria implements Serializable {
@@ -20,7 +19,6 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 
-	@JsonManagedReference 
 	@ManyToMany(mappedBy = "categorias")
 	
 	private List<Produto> produtos = new ArrayList<>();
--- src/main/java/com/projetomc/domain/Pedido.java
+++ src/main/java/com/projetomc/domain/Pedido.java
@@ -18,7 +18,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -32,11 +31,9 @@ public class Pedido implements Serializable {
 	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
 	private Date instante;
 	
-	@JsonManagedReference
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
