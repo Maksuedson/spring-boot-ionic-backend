c9b7218c2b0bb80fee25bdd9bdc91af1b4415a30
Maksuedson <maksuedson@hotmail.com>
2020-03-23 09:27:38 -0300

    Listando todas categorias com DTO

--- src/main/java/com/projetomc/resources/CategoriaResource.java
+++ src/main/java/com/projetomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.projetomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projetomc.domain.Categoria;
+import com.projetomc.dto.CategoriaDTO;
 import com.projetomc.services.CategoriaService;
 
 
@@ -47,6 +50,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO (obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/projetomc/services/CategoriaService.java
+++ src/main/java/com/projetomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.projetomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,10 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é posível excluir uma categoria que ja existe produtos");
 		}
+	}
+
+	public List<Categoria> findAll() {		
+		return repo.findAll();
 	}
 	
 	
