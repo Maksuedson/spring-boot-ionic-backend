dbf96847b2292ffd179e9e42faa829c6e4caa5b9
Maksuedson <maksuedson@hotmail.com>
2020-03-23 21:09:58 -0300

    adicionado configurações para retorno de verbo http correto server.error.include-stacktrace=never, @ResponseStatus(value = HttpStatus.BAD_REQUEST) e @ResponseStatus(value = HttpStatus.NOT_FOUND)

--- src/main/java/com/projetomc/services/exceptions/DataIntegrityException.java
+++ src/main/java/com/projetomc/services/exceptions/DataIntegrityException.java
@@ -1,5 +1,9 @@
 package com.projetomc.services.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class DataIntegrityException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 
