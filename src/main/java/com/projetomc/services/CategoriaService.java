86c6928ac0e14a74e84641304ed6c7e5f1954d70
Maksuedson <maksuedson@hotmail.com>
2020-03-25 11:14:34 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/projetomc/services/CategoriaService.java
+++ src/main/java/com/projetomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.projetomc.domain.Categoria;
@@ -45,6 +48,12 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {		
 		return repo.findAll();
+	}
+	
+	@SuppressWarnings("deprecation")
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 	
