a435d56a3105d6d77ccf716ce6932e0dcd805750
Maksuedson <maksuedson@hotmail.com>
2020-04-10 20:25:19 -0300

    Criando o profile de teste

---
+++ src/main/java/com/projetomc/config/TestConfig.java
@@ -0,0 +1,32 @@
+package com.projetomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.projetomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	
+	
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		
+		
+		
+		
+		return true;
+	}
+
+}
